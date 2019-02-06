6b4d79287b59e84ab5435076a1f6835d2a43d085
lawrie <lawrie.griffiths@ntlworld.com>
2019-02-06 00:17:13 +0000

    Better blinky.ino

--- projects/murax/boothex/blinky.ino
+++ projects/murax/boothex/blinky.ino
@@ -1,9 +1,23 @@
 #define LED (*(volatile uint32_t*)0xF0000004)
+#define UART (*(volatile uint32_t*)0xF0010000)
+#define PWM (*(volatile uint32_t*)0xF0030000)
+
+void putch(char c) {
+  UART = c;
+}
+
+void print(const char *p) {
+  while (*p) putch(*(p++));
+}
+
 int ledState = 0;
 int previousMillis = 0;
 const long interval = 100000;
 
-void setup() { LED = 0xFF; }
+void setup() { 
+  LED = 0xFF;
+  PWM = 191;
+}
 
 void loop()
 {
@@ -15,5 +29,6 @@ if (currentMillis - previousMillis >= interval)
     if (ledState == 0) ledState = 0x0F;
     else ledState = ledState / 2;
     LED = ledState;
+    print("\Blink\n");
   }
 }
